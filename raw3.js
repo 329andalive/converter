f1885a21d0815b9d8468a42518c00ea1b71df8fd
Jeremy <arc7700@gmail.com>
2018-11-05 08:20:56 -0500

    Test code for github

---
+++ raw3.js
@@ -0,0 +1,22 @@
+var  yards = 100
+var meters 
+var MM
+
+
+function convertLength() {
+    if (yards > 0 )
+    //converts yards to meters and inches
+    {
+        console.log((yards * .914).toFixed(3)+ " Meter\n"  + (yards * 36) +' Inches');
+    } else if (meters > 0)
+    //converts meters to yards and inches
+     {
+        console.log((meters * 1.093).toFixed(3) + " Yards\n" + (meters * 39.37).toFixed(3) + " Inches");
+    } else if (inches > 0) 
+    //converts inches to yards and meters.
+       { 
+           console.log((inches / 36).toFixed(3) + ' Yards\n' + (inches / 39.37).toFixed(3) + ' Meter');
+       }
+       else console.log("Input #");
+}
+convertLength();
